8736883f1bc65fdc627278aa13a1983dcd7f00d6
Nyan Cat <17372086+e666666@users.noreply.github.com>
2021-04-11 15:15:15 +0800

    Save the database formatted

--- lib/db.py
+++ lib/db.py
@@ -15,7 +15,7 @@ def load():
 
 def save(data):
   with open(os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__))), "videos.json"), "w") as f:
-    json.dump(data, f)
+    json.dump(data, f, sort_keys=True, indent=2)
     print("Database saved")
 
 
