05b46b3980db5cc43f4a89818b4a7ebe17eb3bbd
Nyan Cat <17372086+e666666@users.noreply.github.com>
2021-04-11 19:37:25 +0800

    Added channel: Systina

--- lib/api.py
+++ lib/api.py
@@ -24,7 +24,7 @@ channels = ["UCshG1-oUuFknWjXp5U1P9xw", "UCjchy66Q6XBGOTSj-jwNy1Q", "UCmC13e647V
             "UCyNA3mQpMAQ59wn0A4uMb5w", "UC4VV8oMlE1A7HEm-hLL5jOg", "UCEddH3sFwOD0AUohA7S06Gg",
             "UC2pRD-iKpUas0o1PSlRLKPg", "UCXUd33vRPGZcfb8pngQpVlQ", "UCVODOPBpytFipYx8iFVZfJA",
             "UC3Cmic0GwevYwxJv96_oUNw", "UCvBZoxNTEr9558Kxzd_QnRg", "UClGm4dxaLMmb2MkNueaoCaw",
-            "UCTSKhJyHFTdR8IVpUktctTw"]
+            "UCTSKhJyHFTdR8IVpUktctTw", "UCxG8IWOc_Go_51HmAUIvpBA"]
 
 
 def ensureAPI():
