049de3ac37db5f5c2c157d8fdb02bacc8b0cd7a6
Nyan Cat <17372086+e666666@users.noreply.github.com>
2021-06-04 12:11:38 +0800

    Add channel: Erik R

--- lib/api.py
+++ lib/api.py
@@ -31,7 +31,7 @@ channels = ["UCshG1-oUuFknWjXp5U1P9xw", "UCjchy66Q6XBGOTSj-jwNy1Q", "UCmC13e647V
             "UCLhmehsjp86fVEfVj5PiT7A", "UCBByZNXMUzz-0qrLwMk9bYg", "UCCfuUsRqZcIAkq-9YEg2e2w",
             "UC8sD3GQrTbJTMMp_NU9sdag", "UCIRjbgsjh_iu_vSl9dtwIDg", "UCG-Gza8DUZOTGVp1ZYqKqjQ",
             "UCqfxtogFDBa6QzsuhG9mFbw", "UCgY9jsdQQa0DkCx07cGR8yA", "UCA54HCOWOdw7uZgMy3XJg4Q",
-            "UCxup3fupRQvocZixof9Y7Vg", "UCatxW9eXbvzkCTgySV-Pkmg"]
+            "UCxup3fupRQvocZixof9Y7Vg", "UCatxW9eXbvzkCTgySV-Pkmg", "UCsh5TdVoO51_DjGyCFIhZsg"]
 
 
 def ensureAPI():
